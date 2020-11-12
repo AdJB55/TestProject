d1446a5fcdeda63ebd90f59f3bfcd1f425de3a3e
Student   BINMORE Adam <05109@farnborough.ac.uk>
2020-11-12 14:15:27 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String greeting = "Hello world";
+
+        System.out.println(greeting);
+
+        System.out.println(greeting.length());
+    }
+}
+ 
